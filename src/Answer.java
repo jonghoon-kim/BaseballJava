9ff118d295c2951bddc6362ea76eef058f579f73
cplkimth <cplkimth@naver.com>
2020-04-28 16:15:43 +0900

    .

---
+++ src/Answer.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Answer {
+    private int[] numbers = new int[Main.DIGIT];
+
+    void generate(){
+        Random random = new Random();
+
+        while (true) {
+            for (int i = 0; i < Main.DIGIT; i++)
+                numbers[i] = random.nextInt(Main.MAX_NUMBER); // liternal
+
+            if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[0])
+                break;
+        }
+    }
+
+    void print() {
+        System.out.println("[정답]");
+        for (int i = 0; i < Main.DIGIT; i++)
+            System.out.print(numbers[i] + " ");
+        System.out.println();
+    }
+
+    public int at(int i) {
+        return numbers[i];
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,29 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
 
     public static final int MAX_NUMBER = 10;
     public static final int DIGIT = 3;
 
     public static void main(String[] args) {
-        int[] answers = Module.generateAnswers();
-        Module.printNumbers("[정답]", answers);
+        Answer answer = new Answer();
+        answer.generate();
+        answer.print();
 
         int tryCount = 0;
         while (true){
             tryCount++;
 
-            int[] guesses = Module.inputGuesses();
-            Module.printNumbers("[추측]", guesses);
+            Guess guess = new Guess();
+            guess.input();
+            guess.print();
 
             Result result = new Result();
-            calculateResult(result, answers, guesses);
-            printResult(result);
+            result.calculate(answer, guess);
+            result.print();
 
-            if (isGoodResult(result))
+            if (result.isGood())
                 break;
         }
 
         System.out.println("도전횟수 : " + tryCount);
-    }
-
-    /**
-     * 결과를 계산한다.
-     * @param result 계산할 결과
-     * @param answers
-     * @param guesses
-     */
-    private static void calculateResult(Result result, int[] answers, int[] guesses) {
-        for (int i = 0; i < DIGIT; i++) {
-            int j = (i + 1) % DIGIT; // (2+1) % 3 = 0
-            int k = (i + 2) % DIGIT; // (2+2) % 3 = 1
-
-            if (guesses[i] == answers[i])
-                result.strike++;
-            else if (guesses[i] == answers[j] || guesses[i] == answers[k])
-                result.ball++;
-            else
-                result.out++;
-        }
-    }
-
-    private static void printResult(Result result) {
-        System.out.println("S:" + result.strike + " B:" + result.ball + " O:" + result.out);
-    }
-
-    private static boolean isGoodResult(Result result) {
-        return result.strike == DIGIT;
     }
 }
