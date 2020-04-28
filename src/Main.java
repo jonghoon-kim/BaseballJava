9c7a43484025cac24afcf96a568c596f87260ed6
cplkimth <cplkimth@naver.com>
2020-04-28 11:51:55 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
         // 정답을 생성한다. (0 ~9 사이의 중복되지 않는 3개의 숫자)
-        int answer0;
-        int answer1;
-        int answer2;
+        int[] answers = new int[DIGIT];
 
         // base class library(API)  vs. 3rd-party library
         Random random = new Random();
 
         while (true) {
-            answer0 = random.nextInt(MAX_NUMBER); // liternal
-            answer1 = random.nextInt(MAX_NUMBER);
-            answer2 = random.nextInt(MAX_NUMBER);
-
-            if (answer0 != answer1 && answer1 != answer2 && answer2 != answer0)
+            for (int i = 0; i < DIGIT; i++)
+                answers[i] = random.nextInt(MAX_NUMBER); // liternal    
+            
+            if (answers[0] != answers[1] && answers[1] != answers[2] && answers[2] != answers[0])
                 break;
         }
 
         System.out.println("[정답]");
-        System.out.print(answer0 + " ");
-        System.out.print(answer1 + " ");
-        System.out.print(answer2 + " ");
+        for (int i = 0; i < DIGIT; i++)
+            System.out.print(answers[i] + " ");
         System.out.println();
 
 
@@ -37,19 +33,15 @@ public class Main {
             tryCount++;
 
             // 추측을 입력한다.
-            int guess0;
-            int guess1;
-            int guess2;
+            int[] guesses = new int[DIGIT];
 
             Scanner scanner = new Scanner(System.in);
-            guess0 = scanner.nextInt();
-            guess1 = scanner.nextInt();
-            guess2 = scanner.nextInt();
+            for (int i = 0; i < DIGIT; i++)
+                guesses[i] = scanner.nextInt();
 
             System.out.println("[추측]");
-            System.out.print(guess0 + " ");
-            System.out.print(guess1 + " ");
-            System.out.print(guess2 + " ");
+            for (int i = 0; i < DIGIT; i++)
+                System.out.print(guesses[i] + " ");
             System.out.println();
 
 
@@ -58,26 +50,17 @@ public class Main {
             int ball = 0;
             int out = 0;
 
-            if (guess0 == answer0)
-                strike++;
-            else if (guess0 == answer1 || guess0 == answer2)
-                ball++;
-            else
-                out++;
+            for (int i = 0; i < DIGIT; i++) {
+                int j = (i + 1) % DIGIT; // (2+1) % 3 = 0
+                int k = (i + 2) % DIGIT; // (2+2) % 3 = 1
 
-            if (guess1 == answer1)
-                strike++;
-            else if (guess1 == answer2 || guess1 == answer0)
-                ball++;
-            else
-                out++;
-
-            if (guess2 == answer2)
-                strike++;
-            else if (guess2 == answer0 || guess2 == answer1)
-                ball++;
-            else
-                out++;
+                if (guesses[i] == answers[i])
+                    strike++;
+                else if (guesses[i] == answers[j] || guesses[i] == answers[k])
+                    ball++;
+                else
+                    out++;
+            }
 
             System.out.println("S:" + strike + " B:" + ball + " O:" + out);
 
