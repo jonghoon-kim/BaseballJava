7ccd24ad4837c919fa744b67856464c2a3633bf5
cplkimth <cplkimth@naver.com>
2020-04-29 09:04:16 +0900

    .

--- src/Answer.java
+++ src/Answer.java
@@ -8,7 +8,7 @@ public class Answer {
 
         while (true) {
             for (int i = 0; i < Main.DIGIT; i++)
-                numbers[i] = random.nextInt(Main.MAX_NUMBER); // liternal
+                numbers[i] = random.nextInt(Main.MAX_NUMBER); // literal
 
             if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[0])
                 break;
